93a55bfb0dc70c22ed71fe4aaae253641882360b
Akiicat <aaaa1379@gmail.com>
2018-08-19 02:18:32 +0800

    remove @google-cloud/debug-agent

--- config/initializers/google-debug.js
+++
@@ -1,9 +0,0 @@
-var pjson = require(env.root + '/package.json');
-
-require('@google-cloud/debug-agent').start({
-  ...env.google.tracer,
-  serviceContext: {
-    service: pjson.name,
-    version: pjson.version,
-  },
-});
